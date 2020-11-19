74f7422c0c80590a81b3349a87f20e7bdd16753d
Sascha Theves <sascha@theves.io>
2020-11-19 16:46:14 +0100

    map src to wp template folder in docker

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export interface AppProps {
+    config?: any;
+}
+const App = (props: AppProps) => {
+    return <div>hello wordpress!</div>;
+};
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(null, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
